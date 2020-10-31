16b2d1d6923532964e9865653f6ade319976a81b
paszkevi4 <paszkevi4@gmail.com>
2020-10-31 13:35:04 +0200

    basic routing inited

---
+++ src/components/Character.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Character = () => {
+  return (
+    <div>
+      Character
+      <NavLink to="/planets">planets</NavLink>
+    </div>
+  );
+};
+
+export default Character;
